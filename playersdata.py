8a7970a7156d4587c3861be38e652d3d749e4591
Evelina Stonyte <stonyteevelina@gmail.com>
2023-04-26 20:42:35 +0300

    Update

--- playersdata.py
+++ playersdata.py
@@ -1,44 +1,53 @@
 import requests
 import json
+from datetime import datetime, timedelta
 import time
 
-class Players():
-    def __init__(self, name, surname, height_inches, height_feet):
+class Player():
+    def __init__(self, name, surname, pts, reb, stl):
         self.name = name
         self.surname = surname
-        self.height_inches = height_inches
-        self.height_feet = height_feet
-    
-    def player_data(self):
-        return f'{self.name}, {self.surname}, {self.height_inches}, {self.height_feet}\n'
+        self.pts = pts
+        self.reb = reb
+        self.stl = stl
 
-with open('nbaplayers.txt', 'w') as file:
-    file.write('Name,Surname,Height(inches),Height(feet)\n')
+    def player_data(self):
+        return f'{self.name}, {self.surname}, {self.pts}, {self.reb}, {self.stl}\n'
+
+yesterday_datetime = datetime.now() - timedelta(days=1)
+yesterday_date = yesterday_datetime.strftime('%Y-%m-%d')
+
+with open('%s.txt' % yesterday_date, 'w') as file:
     
     page_number = 0
     while True:
         page_number += 1
-        query = {'page': str(page_number), 'per_page': '25'}
+        query = {'page': str(page_number), 'per_page': '25', 'dates': str(yesterday_date)}
 
         try:
-            response_API = requests.get('https://www.balldontlie.io/api/v1/players', timeout=50, params=query)
+            response_API = requests.get('https://www.balldontlie.io/api/v1/stats', timeout=50, params=query)
             response_API.raise_for_status()
         except requests.exceptions.RequestException as err:
-                print(err)
-                time.sleep(20)
+            print(err)
+            time.sleep(20)
 
         try:
             playersData = json.loads(response_API.text)
         except json.JSONDecodeError as err:
             print(err)
             break
-        
+
+        playersData = json.loads(response_API.text)
+      
         for item in playersData['data']:
-            if item['height_inches'] == None:
-                item['height_inches'] = ''
-            if item['height_feet'] == None:
-                item['height_feet'] = ''
-            players = Players(item['first_name'], item['last_name'], item['height_inches'], item['height_feet'])
+            if item['pts'] is None:
+                item['pts'] = ''
+            if item['reb'] is None:
+                item['reb'] = ''
+            if item['stl'] is None:
+                item['stl'] = ''  
+            players = Player(item['player']['first_name'], item['player']['last_name'], item['pts'], item['reb'], item['stl']) 
             file.write(players.player_data())
     
+
     file.close()
