8f8fbc3d6765693a43f7102e6b1144a8e62f1c0d
Evelina Stonyte <stonyteevelina@gmail.com>
2023-04-27 09:36:31 +0300

    Corrected

--- games.py
+++ games.py
@@ -3,7 +3,7 @@ import json
 from datetime import datetime, timedelta
 import time
 
-class Player():
+class Games():
     def __init__(self, name, surname, pts, reb, stl):
         self.name = name
         self.surname = surname
@@ -11,16 +11,15 @@ class Player():
         self.reb = reb
         self.stl = stl
 
-    def player_data(self):
+    def games_data(self):
         return f'{self.name}, {self.surname}, {self.pts}, {self.reb}, {self.stl}\n'
 
 yesterday_datetime = datetime.now() - timedelta(days=1)
 yesterday_date = yesterday_datetime.strftime('%Y-%m-%d')
 
-with open('%s.txt' % yesterday_date, 'w') as file:
-    
-    page_number = 0
-    while True:
+page_number = 0
+while True:
+    with open('%s.txt' % yesterday_date, 'a') as file:
         page_number += 1
         query = {'page': str(page_number), 'per_page': '25', 'dates': str(yesterday_date)}
 
@@ -32,22 +31,20 @@ with open('%s.txt' % yesterday_date, 'w') as file:
             time.sleep(20)
 
         try:
-            playersData = json.loads(response_API.text)
+            gamesData = json.loads(response_API.text)
         except json.JSONDecodeError as err:
             print(err)
+            file.close()
             break
-
-        playersData = json.loads(response_API.text)
-      
-        for item in playersData['data']:
+        
+        for item in gamesData['data']:
             if item['pts'] is None:
                 item['pts'] = ''
             if item['reb'] is None:
                 item['reb'] = ''
             if item['stl'] is None:
                 item['stl'] = ''  
-            players = Player(item['player']['first_name'], item['player']['last_name'], item['pts'], item['reb'], item['stl']) 
-            file.write(players.player_data())
-    
+            games = Games(item['player']['first_name'], item['player']['last_name'], item['pts'], item['reb'], item['stl']) 
+            file.write(games.games_data())
 
-    file.close()
+file.close()
